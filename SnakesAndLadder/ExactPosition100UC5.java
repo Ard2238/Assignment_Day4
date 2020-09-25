c5df1d29bbf78c6a26e3a4a92a441eaee249ded9
Ard2238 <abhishekrdas28@gmail.com>
2020-09-25 18:00:28 +0530

    Reapeating till 100, If Player gets above 100: No Further Movement

---
+++ SnakesAndLadder/ExactPosition100UC5.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+
+public class RepeatTill100UC4{
+	
+	static final int ladder = 1, snake = 2;
+	static int moves = 0;
+
+	public static int getNumber(int a){
+		Random rand = new Random();
+		int max =a;
+		int num = rand.nextInt(max);
+
+		return num;
+	}
+
+	public static int checkOptions() {
+		int num = getNumber(3);
+		System.out.println("........" +num);
+		return num;
+	}
+
+	public static void movement(int option, int steps){
+
+		switch (option){
+			case ladder:	
+				if (moves + steps <=100)
+					moves += steps;
+				break;
+			case snake:
+				if(moves -steps >= 0)
+					moves -= steps;
+				else
+					moves = 0;
+				break;
+			default: 
+		}
+
+		System.out.println("The total moves is " + moves);
+	}
+	public static void main(String[] args){
+
+		int num = getNumber(6) + 1;
+		System.out.println("The number obtained is: " + num);
+
+		int option = checkOptions();
+		System.out.println("The option obtained is: " + option);	
+		movement(option, num);
+		
+	}
+}
